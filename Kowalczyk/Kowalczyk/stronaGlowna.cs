8d419778a3c7c3ba388e2a0367bfe8ed3303fa32
Czechuuu <johnybi25@gmail.com>
2023-03-16 18:13:57 +0100

    asd

--- Kowalczyk/Kowalczyk/stronaGlowna.cs
+++ Kowalczyk/Kowalczyk/stronaGlowna.cs
@@ -29,40 +29,5 @@ namespace Kowalczyk
             btnZaloguj.Text = "Wyloguj";
             this.Show();
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Form2 f2 = new Form2();
-            f2.Show();
-            this.Hide();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Form3 f3 = new Form3();
-            f3.Show();
-            this.Hide();
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            Form4 f4 = new Form4();
-            f4.Show();
-            this.Hide();
-        }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            Form5 f5 = new Form5();
-            f5.Show();
-            this.Hide();
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            Form6 f6 = new Form6();
-            f6.Show();
-            this.Hide();
-        }
     }
 }
