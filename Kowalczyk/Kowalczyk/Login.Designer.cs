6a7c903517a1ea86d3fa886d35cb2b405968f6e6
Okuu <110065909+nuclearburb@users.noreply.github.com>
2023-05-08 10:29:39 +0200

    ukryj haslo

--- Kowalczyk/Kowalczyk/Login.Designer.cs
+++ Kowalczyk/Kowalczyk/Login.Designer.cs
@@ -41,10 +41,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(419, 98);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(314, 80);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(223, 46);
+            this.label1.Size = new System.Drawing.Size(181, 37);
             this.label1.TabIndex = 0;
             this.label1.Text = "Logowanie";
             // 
@@ -52,10 +51,9 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(124, 201);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(93, 163);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(245, 29);
+            this.label2.Size = new System.Drawing.Size(196, 24);
             this.label2.TabIndex = 1;
             this.label2.Text = "Nazwa użytkownika:";
             // 
@@ -63,35 +61,32 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(124, 270);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(93, 219);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(90, 29);
+            this.label3.Size = new System.Drawing.Size(73, 24);
             this.label3.TabIndex = 2;
             this.label3.Text = "Hasło:";
             // 
             // user
             // 
-            this.user.Location = new System.Drawing.Point(393, 206);
-            this.user.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.user.Location = new System.Drawing.Point(295, 167);
             this.user.Name = "user";
-            this.user.Size = new System.Drawing.Size(291, 22);
+            this.user.Size = new System.Drawing.Size(219, 20);
             this.user.TabIndex = 3;
             // 
             // pass
             // 
-            this.pass.Location = new System.Drawing.Point(393, 270);
-            this.pass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pass.Location = new System.Drawing.Point(295, 219);
             this.pass.Name = "pass";
-            this.pass.Size = new System.Drawing.Size(291, 22);
+            this.pass.PasswordChar = '*';
+            this.pass.Size = new System.Drawing.Size(219, 20);
             this.pass.TabIndex = 4;
             // 
             // button_login
             // 
-            this.button_login.Location = new System.Drawing.Point(585, 352);
-            this.button_login.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button_login.Location = new System.Drawing.Point(439, 286);
             this.button_login.Name = "button_login";
-            this.button_login.Size = new System.Drawing.Size(100, 28);
+            this.button_login.Size = new System.Drawing.Size(75, 23);
             this.button_login.TabIndex = 5;
             this.button_login.Text = "Zaloguj";
             this.button_login.UseVisualStyleBackColor = true;
@@ -99,10 +94,9 @@
             // 
             // button_exit
             // 
-            this.button_exit.Location = new System.Drawing.Point(129, 457);
-            this.button_exit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button_exit.Location = new System.Drawing.Point(97, 371);
             this.button_exit.Name = "button_exit";
-            this.button_exit.Size = new System.Drawing.Size(100, 28);
+            this.button_exit.Size = new System.Drawing.Size(75, 23);
             this.button_exit.TabIndex = 6;
             this.button_exit.Text = "Wyjście";
             this.button_exit.UseVisualStyleBackColor = true;
@@ -110,9 +104,9 @@
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button_exit);
             this.Controls.Add(this.button_login);
             this.Controls.Add(this.pass);
@@ -120,7 +114,6 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Login";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Login_Load);
