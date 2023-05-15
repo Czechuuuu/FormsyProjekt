d515f152a9a1ea9694cb2ea10cc34d6b6220e875
Okuu <110065909+nuclearburb@users.noreply.github.com>
2023-05-15 12:57:45 +0200

    fix

--- Kowalczyk/Kowalczyk/UserControl/stronaglownaRestaur.Designer.cs
+++ Kowalczyk/Kowalczyk/UserControl/stronaglownaRestaur.Designer.cs
@@ -38,11 +38,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Caveat", 59.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 59.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(337, 26);
+            this.label1.Location = new System.Drawing.Point(187, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(738, 206);
+            this.label1.Size = new System.Drawing.Size(959, 182);
             this.label1.TabIndex = 1;
             this.label1.Text = "     Najlepsze i najtańsze\r\n restauracje w Trójmieście\r\n";
             // 
@@ -58,11 +58,11 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Caveat", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.Black;
             this.label2.Location = new System.Drawing.Point(15, 326);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(630, 82);
+            this.label2.Size = new System.Drawing.Size(835, 73);
             this.label2.TabIndex = 2;
             this.label2.Text = "Tylko u nas tanio i smacznie\r\n";
             // 
